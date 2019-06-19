671cf6a6dbf95b8d1bf15ad8ca3a88d609a5eca2
Josh <dacaboo@yahoo.com>
2019-06-19 09:44:13 -0500

    Completed steps 5 and 6 from instructions.

--- StringCalculator/Program.cs
+++ StringCalculator/Program.cs
@@ -10,15 +10,19 @@ namespace StringCalculator
     {
         static int Add(string numbers)
         {
+            // if empty string, we done
             if (numbers == "")
             {
                 return 0;
             }
 
+            // build a separator list
             List<string> separatorList = new List<string>();
             separatorList.Add(",");
             separatorList.Add("\n");
 
+            // if - operator included a new delimiter, then extract it and then split the string apart
+            // else - just split the string apart
             List<string> splitString = new List<string>();
             if (numbers.Substring(0,2) == "//")
             {
@@ -34,19 +38,44 @@ namespace StringCalculator
                 splitString = numbers.Split(separatorList.ToArray(), StringSplitOptions.RemoveEmptyEntries).ToList();
             }
 
+            // loop through all of the numbers
+            //      if - any are negative, add to a separate list for later
+            //      else - add integer to sum but discard integer if greater than 1000
             int sum = 0;
+            List<int> negs = new List<int>();
             foreach(string s in splitString)
             {
-                sum += Convert.ToInt32(s);
+                int x = Convert.ToInt32(s);
+
+                if (x < 0)
+                {
+                    negs.Add(x);
+                }
+                else
+                {
+                    if (x <= 1000)
+                    {
+                        sum += x;
+                    }
+                }
             }
 
+            // if we found any negatives, then build an exception message and throw the exception
+            if (negs.Count > 0)
+            {
+                string joinednegs = string.Join(",", negs);
+                string exmessage = "negatives not allowed: " + joinednegs;
+                throw new Exception(exmessage);
+            }
+
+            // done
             return sum;
         }
 
         static void Main(string[] args)
         {
-            var numberString = Convert.ToString(Console.ReadLine());
-            int sum = Add(numberString);
+            // test an input string here
+            int sum = Add("//;" + '\n' + "1,2;3" + '\n' + "4,5,1000, 1001");
 
             Console.WriteLine("The sum is: {0}", sum);
             Console.ReadLine();
