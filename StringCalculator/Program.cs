76fe24c5f9a2c2358ca94538974ddb499fd62e0c
Josh <dacaboo@yahoo.com>
2019-06-19 06:52:49 -0500

    Completed steps 1 and 2 from instructions.

---
+++ StringCalculator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator
+{
+    class Program
+    {
+        static int Add(string numbers)
+        {
+            if (numbers == "")
+            {
+                return 0;
+            }
+
+            string[] test = numbers.Split(',');
+
+            int sum = 0;
+            foreach(string s in test)
+            {
+                sum += Convert.ToInt32(s);
+            }
+
+            return sum;
+        }
+
+        static void Main(string[] args)
+        {
+            var numberString = Convert.ToString(Console.ReadLine());
+            int sum = Add(numberString);
+
+            Console.WriteLine("The sum is: {0}", sum);
+            Console.ReadLine();
+        }
+    }
+}
