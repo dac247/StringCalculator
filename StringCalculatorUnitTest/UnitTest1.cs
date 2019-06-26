5e67ae3a2ad0eaaa3265c9d88a3598ec2e466889
Josh <dacaboo@yahoo.com>
2019-06-26 12:19:50 -0500

    Added unit tests for remaining questions.

--- StringCalculatorUnitTest/UnitTest1.cs
+++ StringCalculatorUnitTest/UnitTest1.cs
@@ -46,20 +46,92 @@ namespace StringCalculatorUnitTest
         [TestMethod]
         public void Test5()
         {
-            // test: unknown number of numbers
-            int expected = 12;
-            int test = Program.Add("6,2,4");
+            // test: allow new lines and commas to separate
+            int expected = 6;
+            int test = Program.Add("1" + '\n' + ",2,3");
             Assert.AreEqual(expected, test);
         }
 
+        [TestMethod]
+        public void Test6()
+        {
+            // test: allow new delimeters
+            int expected = 3;
+            int test = Program.Add("//;" + '\n' + "1;2");
+            Assert.AreEqual(expected, test);
+        }
 
+        [TestMethod]
+        public void Test7()
+        {
+            // test: exception on a negative
+            try
+            {
+                int test = Program.Add("-5");
+                Assert.Fail();
+            }
+            catch(Exception ex)
+            {
+                if (ex.Message != "negatives not allowed: -5")
+                {
+                    Assert.Fail();
+                }
+            }
+        }
 
+        [TestMethod]
+        public void Test8()
+        {
+            // test: exception on a negatives
+            try
+            {
+                int test = Program.Add("-5,-14,3,1,-3");
+                Assert.Fail();
+            }
+            catch (Exception ex)
+            {
+                if (ex.Message != "negatives not allowed: -5,-14,-3")
+                {
+                    Assert.Fail();
+                }
+            }
+        }
 
+        [TestMethod]
+        public void Test9()
+        {
+            // test: ignore numbers greater than 1000
+            int expected = 1032;
+            int test = Program.Add("2,4,8,9,1001,5,1000,4");
+            Assert.AreEqual(expected, test);
+        }
 
+        [TestMethod]
+        public void Test10()
+        {
+            // test: any length delimeter
+            int expected = 6;
+            int test = Program.Add("//[***]" + '\n' + "1***2***3");
+            Assert.AreEqual(expected, test);
+        }
 
+        [TestMethod]
+        public void Test11()
+        {
+            // test: multiple delimeters
+            int expected = 6;
+            int test = Program.Add("//[*][%]" + '\n' + "1*2%3");
+            Assert.AreEqual(expected, test);
+        }
 
-
-
+        [TestMethod]
+        public void Test12()
+        {
+            // test: multiple delimeters of varying lengths
+            int expected = 1022;
+            int test = Program.Add("//[;][***][%&]" + '\n' + "1,2;3" + '\n' + "4,5,1000, 1001%&5***2");
+            Assert.AreEqual(expected, test);
+        }
 
     }
 }
